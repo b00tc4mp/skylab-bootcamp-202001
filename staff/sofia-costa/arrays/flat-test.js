67969664251733316dc125aa1efa4cb87b951f0e
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-01-22 12:12:11 +0100

    added files

---
+++ staff/sofia-costa/arrays/flat-test.js
@@ -0,0 +1,29 @@
+console.log('TEST flat')
+
+var results = [a, b, c]
+
+var a = flat(notArray)
+var b = flat(array1)
+var c = flat(simpleArray)
+
+results.forEach(function (result) {
+    console.assert(result.length === )
+    if (!(result instanceof Array)) {throw new TypeError(array + 'is not an array')}
+})
+
+var _error
+try {
+    forEach([1, 2, 3])
+}
+catch(error) {
+    _error = error
+}
+finally {
+
+}
+
+function MyError (error) {
+    error.message = 
+}
+
+
