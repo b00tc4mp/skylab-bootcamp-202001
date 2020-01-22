da9cb4ce94f346190df152dcf9fe82570dd11333
kike4815 <e.pedros.sanchez@gmail.com>
2020-01-22 12:15:56 +0100

    testing demos

--- staff/enric-pedros/arrays/map/map-demo.js
+++ staff/enric-pedros/arrays/map/map-demo.js
@@ -5,3 +5,5 @@ var a = [1,2,3,4]
 console.log(map(a,function(element){
     return (element + 1) 
 }))
+
+
--- staff/enric-pedros/arrays/map/map.js
+++ staff/enric-pedros/arrays/map/map.js
@@ -1,13 +1,14 @@
 'use strict'
 
+console.log('creates a new arry applying the conditions')
 function map(arr, expresion){
-    var newarray = []
+    var newarray=[]
     for (var i=0; i < arr.length; i++){
-        if(expresion(a[i])){
+                    
+        if(expresion(a[i])!== isNaN){
             newarray[newarray.length] = expresion(a[i])
         }
     }
     return newarray
 }
-
 
