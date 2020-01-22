b37763c702b122e094edd91d826b4afe0ce44a56
Alex Park <alexparkvinyas@gmail.com>
2020-01-22 17:55:53 +0100

    added some iifes

--- staff/alex-park/arrays/for-each.test.js
+++ staff/alex-park/arrays/for-each.test.js
@@ -1,12 +1,28 @@
 'use strict'
 
 console.log("TEST forEach-------------------------------------------------");
-
 console.log("it should return the array with all changes modified by the callback");
-var arr = [1, 2, 3];
-forEach(arr, function(value, index){
-    arr[index]=value*=10;
-});
 
-// console.assert(arr[0] === 11, 'Test value on index 0 should be 10');
-console.assert(arr[1] === 20, 'it should be 20');
+(function() {
+    var arr = [1, 2, 3];
+    forEach(arr, function(value, index){
+        arr[index]=value*=10;
+    });
+    
+    console.assert(arr[0] === 10, 'Test value on index 0 should be 10');
+    console.assert(arr[1] === 20, 'it should be 20');
+})();
+
+(function() {
+    var a = [1, 2, 3, 4, 5];
+    var __error;
+
+    try {
+        forEach(-1, function(){});
+    } catch(error) {
+        __error = error;
+    } finally {
+        console.assert((__error instanceof TypeError),'error should be of type TypeError');
+        console.assert(__error.message === '-1 is not an array', )
+    }
+})();
--- staff/alex-park/arrays/map.test.js
+++ staff/alex-park/arrays/map.test.js
@@ -2,21 +2,49 @@
 console.log("TEST map------------------------------------------------------")
 console.log("it should a new array with the applied function without modifying the original one.");
 
-var a = [1, 2, 3];
-var resultA=map(a, function(value) {return value * 10});
-console.assert(resultA[0]===10, 'The value of index 0 should be 10');
-console.assert(resultA[1]===20, 'The value of index 1 should be 20');
-console.assert(resultA[2]===30, 'The value of index 2 should be 30');
-console.assert(resultA.length===3, 'length should be the same because this method do not modified the original array');
+(function() {
+    var a = [1,2,3];
+    var resultA=map(a, function(value) {return value * 10});
+    console.assert(resultA[0]===10, 'The value of index 0 should be 10');
+    console.assert(resultA[1]===20, 'The value of index 1 should be 20');
+    console.assert(resultA[2]===30, 'The value of index 2 should be 30');
+    console.assert(resultA.length===3, 'length should be the same because this method do not modified the original array');
+    
+    var b= ['a', 'b', 'c'];
+    var resultB = map(b, function(value) {return value += '-hello'});
+    console.assert(resultB[0]==='a-hello', 'The value of index 0 should be a-hello');
+    console.assert(resultB[1]==='b-hello','The value of index 1 should be b-hello');
+    console.assert(resultB[2]==='c-hello','The value of index 2 should be c-hello');
+    var c = ['Alex', 'Aurora', 'Pepito'];
+    
+    var resultC = map(c, function(value) {return `Hello, ${value}!`})
+    console.assert(resultC[0]==='Hello, Alex!','The value of index 0 should be Alex');
+    console.assert(resultC[1]==='Hello, Aurora!','The value of index 1 should be Aurora');
+    console.assert(resultC[2]==='Hello, Pepito!','The value of index 2 should be Pepito');
+})();
 
-var b= ['a', 'b', 'c'];
-var resultB = map(b, function(value) {return value += '-hello'});
-console.assert(resultB[0]==='a-hello', 'The value of index 0 should be a-hello');
-console.assert(resultB[1]==='b-hello','The value of index 1 should be b-hello');
-console.assert(resultB[2]==='c-hello','The value of index 2 should be c-hello');
-var c = ['Alex', 'Aurora', 'Pepito'];
+(function() {
+    var __error;
+    var a = [1,2,3];
+    try {
+        map(a, -1);
+    } catch(error) {
+        __error = error;
+    } finally {
+        console.assert(__error.message === "-1 is not a function", 'message should be  "-1 is not a function"');
+        console.assert((__error instanceof TypeError), 'the error should be of type TypeError');
+    };
+})();
 
-var resultC = map(c, function(value) {return `Hello, ${value}!`})
-console.assert(resultC[0]==='Hello, Alex!','The value of index 0 should be Alex');
-console.assert(resultC[1]==='Hello, Aurora!','The value of index 1 should be Aurora');
-console.assert(resultC[2]==='Hello, Pepito!','The value of index 2 should be Pepito');
+(function() {
+    var __error;
+    try {
+        map('a', function(){});
+    } catch(error) {
+        __error = error;
+    } finally {
+        console.assert(__error.message === "a is not an array", 'the message should be "a is not an array"');
+        console.assert((__error instanceof TypeError), 'the error should be of type TypeError');
+    };
+})();
+
