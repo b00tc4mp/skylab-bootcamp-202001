8674b2b241e3f3d622541396168166af32a03cbb
Alex Park <alexparkvinyas@gmail.com>
2020-01-23 00:25:53 +0100

    added some described tests

--- staff/alex-park/arrays/for-each.test.js
+++ staff/alex-park/arrays/for-each.test.js
@@ -1,28 +1,90 @@
-'use strict'
+describe("forEach", function() {
+    it("should return the given array with all values multiplied by 10", function () {
+        var arr = [1, 2, 3];
+        forEach(arr, function(value, index){
+            arr[index]=value*=10;
+        });
 
-console.log("TEST forEach-------------------------------------------------");
-console.log("it should return the array with all changes modified by the callback");
-
-(function() {
-    var arr = [1, 2, 3];
-    forEach(arr, function(value, index){
-        arr[index]=value*=10;
+        assert(arr[0] === 10, 'Test value on index 0 should be 10');
+        assert(arr[1] === 20, 'it should be 20');
     });
+
+    it("should return the given array with all string values doubled", function() {
+        var array = ['a','b','c','d','e'];
+        forEach(array, function(value, index) {array[index]=value+=value})
+
+        assert(array[0].length === 2, 'it should have a length of 2');
+        assert(array[0] === 'aa', 'the resulting value should be "aa"');
+    })
     
-    console.assert(arr[0] === 10, 'Test value on index 0 should be 10');
-    console.assert(arr[1] === 20, 'it should be 20');
-})();
+    it("should fail if the first argument is not an array, giving a TypeError", function () {
+        (function() {
+            var __error;
+            try {
+                forEach('a', function(){});
+            } catch(error) {
+                __error = error;
+            }
+            assert(__error.message === "a is not an array.", 'error message should be "a is not an array", but you got ' + __error.message);
+            assert(__error instanceof TypeError, 'error should be of type TypeError, but instead it got ' + __error.__proto__.constructor.name);
+        })();
+        
+        (function() {
+            var __error;
+            try {
+                forEach(-1, function(){});
+            } catch(error) {
+                __error = error;
+            }
+            assert(__error.message === "-1 is not an array.", 'error message should be "-1 is not an array", but you got ' + __error.message);
+            assert(__error instanceof TypeError, 'error should be of type TypeError, but instead it got ' + __error.__proto__.constructor.name);
+        })();
 
-(function() {
-    var a = [1, 2, 3, 4, 5];
-    var __error;
+        (function() {
+            var __error;
+            try {
+                forEach(true, function(){});
+            } catch(error) {
+                __error = error;
+            }
+            assert(__error.message === "true is not an array.", 'error message should be "true is not an array", but you got ' + __error.message);
+            assert(__error instanceof TypeError, 'error should be of type TypeError, but instead it got ' + __error.__proto__.constructor.name);
+        })();
+    })
 
-    try {
-        forEach(-1, function(){});
-    } catch(error) {
-        __error = error;
-    } finally {
-        console.assert((__error instanceof TypeError),'error should be of type TypeError');
-        console.assert(__error.message === '-1 is not an array', )
-    }
-})();
+    it("should fail when the second argument is not a function, giving a TypeError", function(){
+        (function() {
+            var __error;
+            try {
+                forEach([1,2,3], -1);
+            } catch(error) {
+                __error = error;
+            }
+            assert(__error.message === "-1 is not a function.", 'error message should be "-1 is not a function", but you got ' + __error.message);
+            assert(__error instanceof TypeError, 'error should be of type TypeError, but instead it got ' + __error.__proto__.constructor.name);
+        })();
+
+        (function() {
+            var __error;
+            try {
+                forEach([1,2,3], 'a');
+            } catch(error) {
+                __error = error;
+            }
+            assert(__error.message === "a is not a function.", 'error message should be "a is not a function", but you got ' + __error.message);
+            assert(__error instanceof TypeError, 'error should be of type TypeError, but instead it got ' + __error.__proto__.constructor.name);
+        })();
+
+        (function() {
+            var __error;
+            try {
+                forEach([1,2,3]);
+            } catch(error) {
+                __error = error;
+            }
+            assert(__error.message === "undefined is not a function.", 'error message should be "undefined is not a function", but you got ' + __error.message);
+            assert(__error instanceof TypeError, 'error should be of type TypeError, but instead it got ' + __error.__proto__.constructor.name);
+        })();
+    })
+
+})
--- staff/alex-park/arrays/push.test.js
+++ staff/alex-park/arrays/push.test.js
@@ -1,21 +1,36 @@
-'use strict'
+describe('push', function() {
+    it("it should return the array's length with the new added value", function() {
+        (function() {
+            var a = [1, 2, 3];
 
-console.log("TEST push-----------------------------------------------");
-console.log("it should return the array's length with the new added value");
-var a = [1, 2, 3];
-push(a, 4);
+            push(a, 4);
+
+            assert(a.length === 4, 'should be length 4');
+            assert(a[0] === 1, 'should value at index 0 be a 1, but instead got ' + a[0]);
+            assert(a[1] === 2, 'should value at index 1 be a 2, but instead got ' + a[1]);
+            assert(a[2] === 3, 'should value at index 2 be a 3, but instead got ' + a[2]);
+            assert(a[3] === 4, 'should value at index 3 be a 4, but instead got ' + a[3]);
+
+            var b = ['a', 'b', 'c'];
+            push(b, 'd');
+            assert(a.length === 4, 'should be length 4');
+            assert(b[0] === 'a', 'should value at index 0 be an a, but instead got ' + b[0]);
+            assert(b[1] === 'b', 'should value at index 1 be a b, but instead got ' + b[1]);
+            assert(b[2] === 'c', 'should value at index 2 be a c, but instead got ' + b[2]);
+            assert(b[3] === 'd', 'should value at index 3 be a d, but instead got ' + b[3]);
+
+            var c = [];
+            push(c); 
+            assert(c.length === 1, 'should be length 1');
+
+        })();
+    })
+
+    
+})
 
 
-// var b = ['a', 'b', 'c'];
-// push(b, 'd');
-// console.log("it should return the array's length with the new added value", b);
 
-// var c = [];
-// push(c);
-// console.log("it should return 1 and the array should have an undefined value", c);
 
-console.assert(a.length === 4, 'should be length 4');
-console.assert(a[0] === 1, 'should value at index 0 be a 1');
-console.assert(a[1] === 2, 'should value at index 1 be a 2');
-console.assert(a[2] === 3, 'should value at index 2 be a 3');
-console.assert(a[3] === 4, 'should value at index 3 be a 4');
+
+
