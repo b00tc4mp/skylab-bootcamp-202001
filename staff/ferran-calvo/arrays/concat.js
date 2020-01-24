943beb404536644f2c1eb2991c0930837ee8ea8d
Ferran Calvo <Ferran@MacBook-Pro-de-Ferran.local>
2020-01-24 09:56:58 +0100

    add array files

--- staff/ferran-calvo/arrays/concat.js
+++ staff/ferran-calvo/arrays/concat.js
@@ -1,4 +1,4 @@
-var array1=['a','b','c'];
+/*var array1=['a','b','c'];
 var array2=['d','e','f'];
 
 function concat(a1, a2){
@@ -7,4 +7,15 @@ function concat(a1, a2){
     }
     return a1;
 }
-console.log(concat(array1, array2));
+console.log(concat(array1, array2));*/
+
+function concat(){
+    var newArray = []
+    for(var i = 0; i<arguments.length; i++){
+        if(!(arguments[i] instanceof Array)) throw new TypeError(arguments[i] + ' is not an array')
+        for(var j = 0; j<arguments[i].length; j++){
+            newArray[newArray.length] = arguments[i][j]
+        }
+    }
+    return newArray;
+} 
--- staff/ferran-calvo/arrays/flat.js
+++ staff/ferran-calvo/arrays/flat.js
@@ -1,14 +1,33 @@
-array = [1, 2, [3, 4], [5, 6,[7,8,9]]];
-var newArray = [];
-function flat(a, depth = 1){debugger
-    for (var i=0; i<a.length; i++){
-        if (a[i] instanceof Array & depth>0){
-            flat(a[i], depth-1);
+//a = [1, 2, [3, 4,[5,6,[7,8,9,[10,11,12]]]]];
+//var newArray = [];
+/*function flat(array, depth = 1){
+    for (var i=0; i<array.length; i++){
+        if (array[i] instanceof Array & depth>0){
+            flat(array[i], depth-1);
         }
         else{
-            newArray[newArray.length]=a[i];
+            newArray[newArray.length]=array[i];
+        }
+    }
+    return newArray;
+
+    }
+    
+}
+console.log(flat(a,3));*/
+  
+'use strict'
+function flat(array, depth = 1) {
+    var newArray = []
+    for(var i = 0; i < array.length; i++) {
+        if(array[i].__proto__.constructor.name === 'Array' && depth > 0) {
+            var subarray = flat(array[i], depth - 1)
+            for(var j = 0; j < subarray.length; j++){
+                newArray[newArray.length] = subarray[j]
+            }
+        } else {
+            newArray[newArray.length] = array[i]
         }
     }
     return newArray
 }
-console.log(flat(array, 2));
