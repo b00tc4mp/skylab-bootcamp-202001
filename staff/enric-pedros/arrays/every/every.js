3ce7fc0a3cab77983d686db9e46b0d461a48092d
kike4815 <e.pedros.sanchez@gmail.com>
2020-01-26 23:31:05 +0100

    testing Murray's methods

--- staff/enric-pedros/arrays/every/every.js
+++ staff/enric-pedros/arrays/every/every.js
@@ -2,7 +2,6 @@
 
 function every(arr, expresion){
     if(!(arr instanceof Array )) throw new TypeError(arr + ' is not Array')
-    //if(typeof expresion !== function(){})
     if(typeof expresion !== 'function') throw new TypeError(expresion + ' is not a Function got ' + typeof expresion + ' instead')
 
     for (var i=0; i < arr.length; i++){
