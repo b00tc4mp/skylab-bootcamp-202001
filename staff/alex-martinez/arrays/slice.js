71b288474f60e7bc8e1607844183f370e353153e
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-01-27 09:56:43 +0100

    commit with murrays

--- staff/alex-martinez/arrays/slice.js
+++ staff/alex-martinez/arrays/slice.js
@@ -1,11 +1,17 @@
 'use strict'
 
-function slice(array,initialIndex,lastIndex){
+function slice(array,initialIndex = 0,lastIndex = 0){
     if(!(array instanceof Array)) throw new TypeError(array + ' is not an array');
     var newArray = [];
-    if(initialIndex === ''){
-        initialIndex = 0;
+    
+    if(initialIndex <= 0){
+        initialIndex += array.length;
     }
+
+    if(lastIndex <= 0){
+        lastIndex += array.length;
+    }
+
     for(var i = initialIndex; i<lastIndex; i++){
         newArray[newArray.length] = array[i];
     }
---
+++ staff/alex-martinez/murray/murray.prototype.every.spec.js
@@ -0,0 +1,4 @@
+var m = new Murray(1,2,3);
+m.every(function(value){
+    return value > 0;
+});
