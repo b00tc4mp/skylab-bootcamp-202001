6d0620a80057c983991c3887272a069a6195bd7c
abousiyd <59477326+abousiyd@users.noreply.github.com>
2020-01-28 21:53:02 +0100

    spa done

--- staff/abdou-bousiyd/googl/main.js
+++ staff/abdou-bousiyd/googl/main.js
@@ -1,36 +1,63 @@
-var form = document.querySelector('form');
+var users = []; // ej: user => { name, surname, username, password }
+var loggedUser = null;
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault()
+var search = createSearch('.search', function (query) {
+    googl(query, function (results) {
+        createResults('.results', results);
+    });
+});
+
+createSearch('.search-2', function (query) {
+    ecosia(query, function (results) {
+        createResults('.results-2', results);
+    });
+});
+
+// createSearch('.search-3', function (query) {
+//     googl(query, function (results) {
+//         createResults('.results-3', results);
+//     });
+// });
+
+
+function removeClass(selector, modifier) {
+    document.querySelector(selector).classList.remove(modifier);
+}
+
+function addClass(selector, modifier) {
+    document.querySelector(selector).classList.add(modifier);
+}
+
+function changeTitle(id, text) {
+    document.getElementById(id).innerText = text
+}
+
+var login = auth('.login', function(username, password) {
+    if (username && password) {
     
-    var query = form.query.value
+        var _user = users.find(function(user){
+            return user.username === username && user.password === password
+        })
 
-    googl(query, function(results) {
-        function makeUL(array) {
-            var list = document.createElement('ul');
-        
-            for (var i = 0; i < array.length; i++) {
-
-                var results = document.createElement('li');
-                var titulo = document.createElement('h3');
-                var description = document.createElement('p');
-
-        
-                titulo.appendChild(document.createTextNode(array[i].title));
-                description.appendChild(document.createTextNode(array[i].description));
-                //titulo.appendChild(document.createTextNode(array[i].title));
-        
-                list.appendChild(results);
-                results.appendChild(titulo);
-                results.appendChild(description);
-                
-
-            }
-        
-            return list;
+        if (_user){
+            loggedUser = _user
+            addClass(".login", 'login--hide')
+            removeClass(".search", 'search--hide')
+            changeTitle("title", "Search")
+            changeTitle("user", "Welcome: " + _user.username)
+        }else{
+            alert('datos incorectos')
         }
-        
-        form.appendChild(makeUL(results));
-    })
-})
 
+    } else alert('Field required');
+});
+
+
+var register = auth('.register', function(username, password) {
+    if (username && password) {
+        users.push({username, password})
+        addClass(".register", 'register--hide')
+        removeClass(".login", 'login--hide')
+        changeTitle("title", "Login")
+    } else alert('Field required');
+});
