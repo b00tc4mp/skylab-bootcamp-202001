7b57d3e7c927df1da334ab129484bd683ca29085
Sofia Costa <58744044+sofsscosta@users.noreply.github.com>
2020-01-28 10:57:55 +0100

    first test with html document

--- staff/sofia-costa/googl/googl.demo.js
+++ staff/sofia-costa/googl/googl.demo.js
@@ -1,4 +1,6 @@
-googl('pepito', function(results) { 
+var input = document.querySelector('input')
+
+googl(input, function(results) { 
 
     results.forEach(function(result) { 
         console.log(result) 
--- staff/sofia-costa/googl/googl.js
+++ staff/sofia-costa/googl/googl.js
@@ -6,7 +6,7 @@ function googl(query, callback) {
     xhr.onreadystatechange = function (res) {
         
         if (this.readyState === 4 && this.status === 200) {
-
+            console.log(this.readyState)
             var doc = new DOMParser().parseFromString(this.responseText, 'text/html')
 
             var items = doc.querySelectorAll('div.g')
@@ -18,7 +18,7 @@ function googl(query, callback) {
 
                 var title = item.querySelector('h3.LC20lb')
 
-                if (title) { debugger
+                if (title) {
                     var result = {}
 
                     result.title = title.innerText
@@ -46,3 +46,64 @@ function googl(query, callback) {
 
     xhr.send()
 }
+
+
+var submit = document.querySelector('button')
+
+
+var input = document.querySelector('input').value
+
+submit.addEventListener('click', function(event) {
+    event.preventDefault()
+
+    var input = document.querySelector('input').value
+    googl(input, function(results) {
+        results.forEach(function(result) { 
+            console.log(result) 
+        })
+    })
+})
+
+// TODO create tests with just console.assert (check that each item has at least a title and a description)
+
+
+
+// googl(input, function(results) { 
+
+//     results.forEach(function(result) { 
+//         console.log(result) 
+//     })
+
+//     console.log('%c should results have a title', 'color: green;')
+//     results.forEach(function(result) {
+//         console.assert(result.title !== undefined, 'result should have a title, but it doesn\'t')
+//    })
+
+//     console.log('%c should results have a description', 'color: green;')
+//     results.forEach(function(result) {
+//         console.assert(result.description !== undefined, 'result should have a description, but it doesn\'t')
+//     })
+
+//     console.log('%c should results show a rating, in case they have one', 'color: green;')
+//     results.forEach(function(result) {
+//         if (results.rating)
+//             console.assert(result.rating !== undefined, 'result should have a description, but it doesn\'t')
+//     })
+
+//     console.log('%c should results have a link', 'color: green;')
+//     results.forEach(function(result) {
+//         console.assert(result.link !== undefined, 'result should have a link, but it doesn\'t')
+//     })
+
+//     console.log('%c should results be objects', 'color: green;')
+//     results.forEach(function(result) {
+//         console.assert(result instanceof Object, 'result should be an object but it isn\'t')
+//     })
+
+//     console.log('%c should each key of results be a string', 'color: green;')
+//     results.forEach(function(result) { 
+//         console.assert(typeof result.title === 'string', 'result should be a string but it isn\'t')
+//         console.assert(typeof result.description === 'string', 'result should be a string but it isn\'t')
+//         if (result.ranking !== undefined) console.assert(typeof result.rating === 'string', 'result should be a string but it isn\'t')
+//     })
+// })
