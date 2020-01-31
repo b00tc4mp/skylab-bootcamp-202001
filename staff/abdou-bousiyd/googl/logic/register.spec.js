819582a3f9561261a1a699a93478e518b1f34e2d
abousiyd <59477326+abousiyd@users.noreply.github.com>
2020-01-31 17:07:15 +0100

    add test

---
+++ staff/abdou-bousiyd/googl/logic/register.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('register', function () {
+    //var user;
+    beforeEach(function () {
+        users.length = 0;
+
+        user = {
+            name: 'name-' + Math.random(),
+            surname: 'surname-' + Math.random(),
+            username: 'username-' + Math.random(),
+            password: 'password-' + Math.random()
+        };
+    });
+
+});
