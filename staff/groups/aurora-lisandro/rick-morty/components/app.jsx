e2ea7792e374c6519af0b97d16b38e2cdae3f44d
Lisandro B <lisandro.bertoli@gmail.com>
2020-02-09 20:22:28 +0100

    fixed app.jsx conflict

---
+++ staff/groups/aurora-lisandro/rick-morty/components/app.jsx
@@ -0,0 +1,26 @@
+const { Component } = React
+class App extends Component {
+    state = {
+        view: undefined
+    }
+    render() {
+        const {
+            props: { title }, state: { view } } = this
+        return <main className='app'>
+
+            {view !== 'login' && view !== 'register' && <Navbar
+                onLogout={() => console.log('logout')}
+                onToSearch={(type) => { console.log(type) }}
+                onToFavs={() => console.log('onToFavs')}
+                onToProfile={() => { console.log('profile') }} />}
+
+            <h1>{title}</h1>
+
+            {view === 'login' && <Login onSubmit={handleLogin} onToRegister={handleRegister} />}
+
+            {view === 'results' && <Results results={console.log('results')} onItemClick={console.log('item')} onItemFavClick={console.log('fav')} />}
+
+            {view === "register" && <Register onSubmit={console.log('submit')} onToLogin={console.log('login')} error={undefined} />}
+        </main>
+    }
+}
