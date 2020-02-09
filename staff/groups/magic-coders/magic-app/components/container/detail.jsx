df337a048fbadcb6878793dab868810f306d42cd
RPXIC <rpc2works@gmail.com>
2020-02-09 18:22:31 +0100

    modified details

--- staff/groups/magic-coders/magic-app/components/container/detail.jsx
+++ staff/groups/magic-coders/magic-app/components/container/detail.jsx
@@ -1,19 +1,16 @@
-function Detail({card: { name, type, text, cmc, imageUrl}}) {
+function Detail({card: { name, number, type, rarity, setName, text, cmc, imageUrl}}) {
 
-  const style = {
-    display: "flex",
-    justifyContent: 'center',
-    alignItems: 'center'
-
-  }
-  return <div>
-    <div style={style}>
-      <h1 style={{marginRight: '10px'}}>{name}</h1>
+  return <div className="detail">
+    <div className="detail__title">
+    <h1 style={{marginRight: '10px'}}>#{number} - {name} -</h1>
     </div>
-    
-    <h3>{type}</h3>
-    <p>{text}</p>
-    <h4>{cmc}</h4>
-    <img src={imageUrl} />
+    <img className="detail__img" src={imageUrl} />
+    <div className="detail__extra">
+      <h4 className="detail__extra__text">Type: {type}</h4>
+      <h4 className="detail__extra__text">Set name: {setName}</h4>
+      <h4 className="detail__extra__text">Mana cost: {cmc}</h4>
+      <h4 className="detail__extra__text">Rarity: {rarity}</h4>
+    </div>
+    <p className="detail__text">{text}</p>
   </div>
 }
