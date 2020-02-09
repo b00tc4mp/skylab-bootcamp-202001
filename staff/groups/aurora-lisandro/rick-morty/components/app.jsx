326c2c7f0d5bf48bf424afb2be608601fa342061
Lisandro B <lisandro.bertoli@gmail.com>
2020-02-09 16:36:12 +0100

    add navbar component

--- staff/groups/aurora-lisandro/rick-morty/components/app.jsx
+++ staff/groups/aurora-lisandro/rick-morty/components/app.jsx
@@ -3,7 +3,7 @@ const { Component } = React
 class App extends Component {
 
     state = {
-        view: 'register'
+        view: undefined
     }
 
 
@@ -12,9 +12,16 @@ class App extends Component {
             props: { title }, state: {view}} = this
 
         return <main className="app">
-            <h1>{title}</h1>
+            {view!=='login' && view!=='register' && <Navbar 
+            onLogout={()=>console.log('logout')} 
+            onToSearch={(type)=>{console.log(type)}} 
+            onToFavs={()=>console.log('onToFavs')} 
+            onToProfile={()=>{console.log('profile')}}/>}
 
+            <h1>{title}</h1>
+            
             {view==="register" && <Register onSubmit={console.log('submit')} onToLogin={console.log('login')} error={undefined}/>}
+
            
 
         </main>
