cafe6c166442666141680b22474b2ae62d34ec20
Lisandro B <lisandro.bertoli@gmail.com>
2020-02-10 16:06:36 +0100

    add favs recognition to search

--- staff/groups/aurora-lisandro/rick-morty/logic/search-characters.js
+++ staff/groups/aurora-lisandro/rick-morty/logic/search-characters.js
@@ -1,21 +1,47 @@
-function searchCharacters(query, callback) {
+function searchCharacters(query, token, callback) {
     if (typeof query !== 'string') throw new TypeError(`query ${query} is not a string`)
     // if (typeof token !== 'string') throw new TypeError(`token ${token} is not a string`)
     if (typeof callback !== 'function') throw new TypeError(`callback ${callback} is not a function`)
 
-
-
-
-    call(`https://rickandmortyapi.com/api/character/?name=${query}`, undefined, (error, response) => {
-        debugger
+    call('http://skylabcoders.herokuapp.com/api/v2/users/', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    }, (error, response) => {
         if (error) return callback(error)
 
-        const content = JSON.parse(response.content)
-
-        const { error: _error, info, results } = content
+        const { error: _error, favs } = JSON.parse(response.content)
 
         if (_error) return callback(new Error(_error))
 
-        if (response.status === 200) return callback(undefined, content)
+
+        call(`https://rickandmortyapi.com/api/character/?name=${query}`, undefined, (error, response) => {
+
+            if (error) return callback(error)
+
+
+            if (response.status === 200) {
+
+                const content = JSON.parse(response.content)
+                const { results } = content
+
+                if (favs) {
+                    results.forEach(character => {
+                        if (favs.includes(character.id)) character.isFav = true
+                    })
+                }
+
+                return callback(undefined, content)
+            }
+
+            const { error: _error } = JSON.parse(response.content)
+
+            if (response.status === 404) return callback(new Error(_error))
+            else return callback(new Error('Unknown error'))
+
+        })
     })
+
 }
--- staff/groups/aurora-lisandro/rick-morty/logic/search-characters.spec.js
+++ staff/groups/aurora-lisandro/rick-morty/logic/search-characters.spec.js
@@ -1,84 +1,151 @@
 describe('searchCharacters', () => {
-    const queryNames = ['rick', 'morty', 'sanchez', 'summer', 'Beth Smith']
-    let query
+    let name, surname, username, password, appTag, token, query, ids
+
+    const queryValues = {
+        'rick sanchez': ['1', '290', '293'],
+        'morty smith': ['2', '232', '234'],
+        'beth': ['4', '47', '38', '39', '40']
+    }
+
 
     beforeEach(() => {
-        query = queryNames.random()
+        name = `name-${Math.random()}`
+        surname = `surname-${Math.random()}`
+        username = `username-${Math.random()}`
+        password = `password-${Math.random()}`
+        appTag = 'Rick1.0'
+
+        query = Object.keys(queryValues).random()
+        ids = queryValues[query]
     })
 
-    it('should succeed on matching query', done => {
-        searchCharacters(query, (error, response) => {
-            const { results, info } = response
+    describe('when user exists', () => {
+        beforeEach(done => {
+            call(`https://skylabcoders.herokuapp.com/api/v2/users`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    name,
+                    surname,
+                    username,
+                    password,
+                    appTag
+                })
+            }, (error, response) => {
+                if (error) return done(error)
 
-            expect(info).toBeDefined()
-            expect(results.length).toBeGreaterThan(0)
+                if (response.content) {
+                    const { error } = JSON.parse(response.content)
 
-            expect(results).toBeDefined()
-            expect(results.length).toBeGreaterThan(0)
+                    if (error) return done(new Error(error))
+                }
 
-            results.forEach(character => {
-                expect(typeof character.id).toBe('number')
-                expect(typeof character.name).toBe('string')
-                expect(typeof character.status).toBe('string')
-                expect(typeof character.species).toBe('string')
-                expect(typeof character.gender).toBe('string')
-                expect(typeof character.origin).toBe('object')
-                expect(typeof character.location).toBe('object')
+                call('https://skylabcoders.herokuapp.com/api/v2/users/auth', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ username, password })
 
-                done()
+                }, (error, response) => {
+                    if (error) return done(error)
+
+                    if (response.content) {
+                        const { error, token: _token } = JSON.parse(response.content)
+
+                        if (error) return done(new Error(error))
+
+                        token = _token
+                        done()
+                    }
+                })
             })
         })
-    })
+        it('should succeed on matching query with false isFavs', done => {
+            searchCharacters(query, token, (error, response) => {
+                const { results, info } = response
 
-    it('should succeed on non-matching query returning error message', done => {
-        searchCharacters('no-existing-charachter', error => {
-            expect(error).toBeInstanceOf(Error)
-            expect(error.message).toBe('There is nothing here')
+                expect(info).toBeDefined()
+                expect(results.length).toBeGreaterThan(0)
 
-            done()
+                expect(results).toBeDefined()
+                expect(results.length).toBeGreaterThan(0)
 
+                results.forEach(character => {
+                    expect(typeof character.id).toBe('number')
+                    expect(typeof character.name).toBe('string')
+                    expect(typeof character.status).toBe('string')
+                    expect(typeof character.species).toBe('string')
+                    expect(typeof character.gender).toBe('string')
+                    expect(typeof character.origin).toBe('object')
+                    expect(typeof character.location).toBe('object')
+                    // expect(typeof character.isFav).toBeFalsy() TODO
+
+
+                    done()
+                })
+            })
         })
+
+        it('should succeed on non-matching query returning error message', done => {
+            searchCharacters('no-existing-charachter', token, error => {
+                expect(error).toBeInstanceOf(Error)
+                expect(error.message).toBe('There is nothing here')
+
+                done()
+
+            })
+        })
+
+        //TODOOO
+
+        // it('should the isFav prop be added to the character', done => {
+        //     searchCharacters(query, token, (error, response) => {
+        //         const { results, info } = response
+
+        //         expect(results)
+        //     })
+        // })
     })
+
 
     it('should fail on non-string query', () => {
         const query = 1
         expect(() => {
-            searchCharacters(query, () => { })
+            searchCharacters(query, token, () => { })
         }).toThrowError(TypeError, `query ${query} is not a string`)
     })
 
     it('should fail on non-string query', () => {
         const query = true
         expect(() => {
-            searchCharacters(query, () => { })
+            searchCharacters(query, token, () => { })
         }).toThrowError(TypeError, `query ${query} is not a string`)
     })
 
     it('should fail on non-string query', () => {
         const query = undefined
         expect(() => {
-            searchCharacters(query, () => { })
+            searchCharacters(query, token, () => { })
         }).toThrowError(TypeError, `query ${query} is not a string`)
     })
 
     it('should fail on non-function callback', () => {
         const callback = 1
         expect(() => {
-            searchCharacters(query, callback)
+            searchCharacters(query, token, callback)
         }).toThrowError(TypeError, `callback ${callback} is not a function`)
     })
 
     it('should fail on non-function callback', () => {
         const callback = true
         expect(() => {
-            searchCharacters(query, callback)
+            searchCharacters(query, token, callback)
         }).toThrowError(TypeError, `callback ${callback} is not a function`)
     })
 
     it('should fail on non-function callback', () => {
         const callback = 'function'
         expect(() => {
-            searchCharacters(query, callback)
+            searchCharacters(query, token, callback)
         }).toThrowError(TypeError, `callback ${callback} is not a function`)
     })
 })
