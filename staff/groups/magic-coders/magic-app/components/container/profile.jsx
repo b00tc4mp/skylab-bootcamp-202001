c90fcd85a8448b92ef93462ae47c77d2a1ba7250
Carlos Matilla <carlosmatilla@MacBook-Pro-de-Carlos-2.local>
2020-02-10 17:57:25 +0100

    detail started

--- staff/groups/magic-coders/magic-app/components/container/profile.jsx
+++ staff/groups/magic-coders/magic-app/components/container/profile.jsx
@@ -1,7 +1,7 @@
 function Profile() {
   return (
-    <div className="profile">
-      <div className="profile__card">
+
+      <div className="profile">
         <header className="profile__header">
           <div className="profile__pic">Foto perfil</div>
           <div className="profile__name">Carlos</div>
@@ -11,9 +11,13 @@ function Profile() {
           </div>
         </header>
         <main className="profile__content">
-         
+         <img src="http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=379007&type=card" alt=""/>
+         <img src="http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=378007&type=card" alt=""/>
+         <img src="http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=374007&type=card" alt=""/>
+         <img src="http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=379307&type=card" alt=""/>
+         <img src="http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=3009&type=card" alt=""/>
         </main>
-      </div>
+    
     </div>
   )
 }
