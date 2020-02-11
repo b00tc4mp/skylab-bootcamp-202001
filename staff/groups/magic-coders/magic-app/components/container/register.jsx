91190365503745fad015007f8c0d2442cda1f1a3
Carlos Matilla <carlosmatilla@MacBook-Pro-de-Carlos-2.local>
2020-02-11 16:08:39 +0100

    phone and email added to register

--- staff/groups/magic-coders/magic-app/components/container/register.jsx
+++ staff/groups/magic-coders/magic-app/components/container/register.jsx
@@ -2,10 +2,12 @@ function Register ({onSubmit, handleGoToLogin, error}) {
 
     return <Form className="register" onSubmit={onSubmit} >
         <H2>Sign-up</H2>
-        <Input name="name" placeholder="name"/>
-        <Input name="surname" placeholder="surname"/>
-        <Input name="username" placeholder="username"/>
-        <Input name="password" placeholder="password" type="password"/>
+        <Input name="name" placeholder="Name" required/>
+        <Input name="surname" placeholder="Surname" required/>
+        <Input name="phone" placeholder="Phone" />
+        <Input name="email" placeholder="Email" type="email" required/>
+        <Input name="username" placeholder="Username" required/>
+        <Input name="password" placeholder="password" type="Password" required/>
         <Button>Register</Button>
         {error && <P>{error}</P>}
         <A onClick={handleGoToLogin}>Login</A>
--- staff/groups/magic-coders/magic-app/logic/register-user.js
+++ staff/groups/magic-coders/magic-app/logic/register-user.js
@@ -1,20 +1,22 @@
 'use strict';
 
-function registerUser({name, surname, username, password}, callback) {
+function registerUser({name, surname, phone, email, username, password}, callback) {
     if (typeof name !== 'string') throw new TypeError('name ' + name + ' is not a string');
     if (!name.trim()) throw new Error('name is empty');
     if (typeof surname !== 'string') throw new TypeError('surname ' + surname + ' is not a string');
     if (!surname.trim()) throw new Error('surname is empty');
-    if (typeof username !== 'string') throw new TypeError('username ' + username + ' is not a string');
-    if (!username.trim()) throw new Error('username is empty');
+    if (typeof phone !== 'string') throw new TypeError(phone + ' is not a number');
+    if (typeof email !== 'string') throw new TypeError(email + ' is not a string');
+    if (!email.trim()) throw new Error('email is empty');
     if (typeof password !== 'string') throw new TypeError('password ' + password + ' is not a string');
     if (!password.trim()) throw new Error('password is empty');
     if (typeof callback !== "function") throw new Error(`${callback} is not a function`)
     const mtg= true
+    debugger
     call(`https://skylabcoders.herokuapp.com/api/v2/users`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, surname, username, password, mtg })
+        body: JSON.stringify({ name, surname, phone, email, username, password, mtg })
     }, (error, response) => {
         // Network Error
         if (error) return callback(error)
