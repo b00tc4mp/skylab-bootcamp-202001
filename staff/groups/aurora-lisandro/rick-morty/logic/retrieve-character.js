b2471b48875982d0f10fe4658fb9278354c04abe
Lisandro B <lisandro.bertoli@gmail.com>
2020-02-13 16:25:36 +0100

    all but one tests fixed

--- staff/groups/aurora-lisandro/rick-morty/logic/retrieve-character.js
+++ staff/groups/aurora-lisandro/rick-morty/logic/retrieve-character.js
@@ -1,6 +1,6 @@
 function retrieveCharacter(token, id, callback) {
     if (typeof token !== 'string') throw new TypeError(`token ${token} is not a string`)
-    // if (typeof id !== 'string') throw new TypeError(`id ${id} is not a string`)
+    if (typeof id !== 'number') throw new TypeError(`id ${id} is not a number`)
     if (typeof callback !== 'function') throw new TypeError(`callback ${callback} is not a function`)
 
     const [header, payload, signature] = token.split('.')
@@ -8,6 +8,8 @@ function retrieveCharacter(token, id, callback) {
     if (!header || !payload || !signature) throw new Error('invalid token')
 
     const { sub } = JSON.parse(atob(payload))
+
+    if (!sub) throw new Error('no id in token')
 
 
     call('https://skylabcoders.herokuapp.com/api/v2/users/', {
