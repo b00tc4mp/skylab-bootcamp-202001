eab953722d2007e18d631efdba9d5523ec343ba9
RPXIC <rpc2works@gmail.com>
2020-02-13 18:03:17 +0100

    doc functions

--- staff/groups/magic-coders/magic-app/logic/add-card-to-sale.js
+++ staff/groups/magic-coders/magic-app/logic/add-card-to-sale.js
@@ -1,4 +1,13 @@
+/** @function addCardToSale */
 function addCardToSale(card, token, callback) {
+console.log(card)
+debugger
+/**
+ * @param {object} card - Contain all properties and values of target card for send data to on sale
+ * @param {string} token - Need token to send targeted card to add new property on actual user
+ * @param {function} callback - Return error / update successful
+ */
+
     if (card.constructor.name !== "Object") throw new TypeError(`card ${card} is not an Object`)
     if (typeof token !== "string") throw new TypeError(`token ${token} is not a string`)
     if (typeof callback !== "function") throw new TypeError(`callback ${callback} is not a function`)
