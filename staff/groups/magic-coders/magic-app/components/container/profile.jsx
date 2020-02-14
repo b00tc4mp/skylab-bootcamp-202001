0ad9010a956956d2c6b846cfa91b99c5940e00b6
Carlos Matilla <carlosmatilla@MacBook-Pro-de-Carlos-2.local>
2020-02-14 11:47:20 +0100

    fixin

--- staff/groups/magic-coders/magic-app/components/container/profile.jsx
+++ staff/groups/magic-coders/magic-app/components/container/profile.jsx
@@ -5,9 +5,9 @@ function Profile({user, cards, view, viewProfile, toggleButton, toSold}) {
         <header className="profile__header">
           <div className="profile__name">{<h2>{firstUppercase(user.name)}</h2>}</div>
           <div className="profile__estadistics">
-            <p>On Sale: {viewProfile ? cards.length: 0}</p>
-            <p>Sold: {viewProfile ? 0 : cards.length}</p>
-            <p>Total Amount: ${getTotalPriceCards(cards)}</p></div>
+            <p>On Sale<br/><br/>{viewProfile ? cards.length: 0}</p>
+            <p>Sold<br/><br/>{viewProfile ? 0 : cards.length}</p>
+            <p>Total Amount<br/><br/>${getTotalPriceCards(cards)}</p></div>
           <div className="profile__buttons">
             <button onClick={toggleButton} className={viewProfile ? 'button--green' : 'button'}>For Sale</button>
             <button onClick={toggleButton} className={viewProfile ? 'button' : 'button--green'}>Sold</button>
