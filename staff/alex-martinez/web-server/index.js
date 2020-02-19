b734697449c02037be005a04630c338830e0c82b
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-02-19 14:26:27 +0100

    modify http server

--- staff/alex-martinez/web-server/index.js
+++ staff/alex-martinez/web-server/index.js
@@ -1,29 +1,51 @@
 const http = require('http')
 const fs = require('fs')
-const url = require('url')
+
 const logger = require('./logger')
 
 
-const server = http.createServer((req,res)=> {
+const server = http.createServer((request,response)=> {
     logger.info('starting server')
     logger.debug('setting encoding to utf8')
 
-    const ip = req.socket.remoteAddress
+    const ip = request.socket.remoteAddress
     logger.info('IP: ' + ip )
 
-    let q = url.parse(req.url, true);
-    let filename = '.' + q.pathname;
-    if(filename === './') filename = 'index.html'
+    let path = req.url
+    if(path === '/') path += 'index.html'
+    path = `.${path}`;
 
-    fs.readFile(filename, function(error, html){
-        if(error) {
-            //res.writeHead(404,{'Content-Type': 'type/html'});
-            return res.end('404 Not Found');
-        }
-        //res.writeHead(200, {'Content-Type':'text/html'});
-        res.write(html);
-        res.end()
-     
+    let rs = fs.createReadStream(path)
+    response.setHeader('Content-Type','text/html')
+
+    /* rs.on('data', chunk => {res.write(chunk)})
+    rs.on('end', ()=> res.end()) */
+    
+    //con pipe 
+    rs.pipe(response)
+
+    rs.on('error', error => {
+        logger.warn(error)
+        res.writeHead(404)
+        return res.end('<h1>Not Found</h1>');
     })
+    response.on('error', error => logger.error(error))
+
+    /* fs.readFile(path, function(error, html){
+        if(error) {
+            res.writeHead(404);
+            return response.end('404 Not Found');
+        }
+        res.setHeader(200, {'Content-Type':'text/html'});
+        res.write(html);
+        response.end()
+     
+    }) */
 })
 server.listen(8000)
+
+process.on('SIGINT', () => {
+    logger.warn(`server abruptly stopped`)
+
+    setTimeout(() => process.exit(0), 1000)
+})
