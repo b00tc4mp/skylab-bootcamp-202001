2c6e5addbb6b046cb5f33abbbca23e43f5a57ec8
RPXIC <rpc2works@gmail.com>
2020-02-19 20:52:46 +0100

    first commit app-server

---
+++ staff/ruben-ponce/app-server/main.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const register = require('./logic/register')
+const users = require('./data')
+
+const app = express()
+
+app.use(express.static('public'));
+app.use(express.static('logic'));
+app.use(express.static('utils'));
+
+app.post('/register', (req, res) => {
+    let body = ''
+    let _body = []
+    
+    req.on('data', chunk => {
+        body += chunk
+    })
+    req.on('end', () => {
+        // DO something with body (debug here, analise it, parse it... etc)
+        // console.log('body =>', body)
+        body = body.split('&')
+        body.forEach(data => {
+            _body.push(data.split('=')[1])
+        });
+        let name = _body[0]
+        let surname = _body[1]
+        let username = _body[2]
+        let password = _body[3]
+
+        register(name, surname, username, password)
+
+        res.end(users)
+
+
+    })
+})
+
+
+app.listen(8080, function () {
+    console.log('server up')
+});
