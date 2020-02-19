4fe7c7c955112ada660ead70adc8457562f01e8c
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-02-19 11:40:01 +0100

    add ip to logger

--- staff/alex-martinez/web-server/index.js
+++ staff/alex-martinez/web-server/index.js
@@ -3,10 +3,13 @@ const fs = require('fs')
 const url = require('url')
 const logger = require('./logger')
 
-logger.info('starting server')
 
 const server = http.createServer((req,res)=> {
+    logger.info('starting server')
     logger.debug('setting encoding to utf8')
+
+    const ip = req.socket.remoteAddress
+    logger.info('IP: ' + ip )
 
     let q = url.parse(req.url, true);
     let filename = '.' + q.pathname;
@@ -14,12 +17,13 @@ const server = http.createServer((req,res)=> {
 
     fs.readFile(filename, function(error, html){
         if(error) {
-            res.writeHead(404,{'Content-Type': 'type/html'});
+            //res.writeHead(404,{'Content-Type': 'type/html'});
             return res.end('404 Not Found');
         }
-        res.writeHead(200, {'Content-Type':'text/html'});
+        //res.writeHead(200, {'Content-Type':'text/html'});
         res.write(html);
-        res.end();
+        res.end()
+     
     })
 })
 server.listen(8000)
