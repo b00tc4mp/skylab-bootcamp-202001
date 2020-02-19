f615d6ac65b03b93aff63ef9a6dbb5948224052b
Federico Lacabaratz <federicolacabaratz@MacBook-Pro-de-Federico.local>
2020-02-19 18:03:18 +0100

    add updates to new challenge app-server

---
+++ staff/federico-lacabaratz/app-server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const logger = require('./utils/logger')
+const path = require('path')
+const loggerMidWare = require('./utils/logger-mid-ware')
+
+const { argv: [, , port = 8080] } = process
+
+logger.level = logger.DEBUG
+logger.path = path.join(__dirname, 'server.log')
+
+logger.debug('setting up server')
+
+const app = express()
+
+app.use(loggerMidWare)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(port, () => logger.info(`server up and running on port ${port}`))
+
+process.on('SIGINT', () => {
+    logger.warn(`server abruptly stopped`)
+
+    process.exit(0)
+})
