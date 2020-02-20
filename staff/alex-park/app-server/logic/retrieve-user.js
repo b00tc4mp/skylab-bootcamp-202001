4741ae26a75575a740291bc36bffa3e5a976c73c
Alex Park <alexparkvinyas@gmail.com>
2020-02-20 16:53:11 +0100

    fixed retrieve-user

--- staff/alex-park/app-server/logic/retrieve-user.js
+++ staff/alex-park/app-server/logic/retrieve-user.js
@@ -3,7 +3,7 @@ const users = require('../data/users')
 module.exports = username => {
     if (typeof username !== 'string') { throw new TypeError(username + " is not a string.") };
 
-    let user = (users.find(function(user) { return user.username === username })).name
+    let user = (users.find(function(user) { return user.username === username }))
 
     return user
 }
