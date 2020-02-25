f90b71b7f4fd27b3b64efd69bba59bd53299e34c
victoriCrediday <victori.murillo.mora@gmail.com>
2020-02-25 17:58:43 +0100

    added pug

---
+++ staff/victori-murillo/app-server/_7/routes/landing.js
@@ -0,0 +1,28 @@
+const { retrieveUser } = require('../logic')
+const { App, Landing } = require('../components')
+const { logger } = require('../utils')
+
+module.exports = ({ session: { token, acceptCookies } }, res) => {
+    if (token) {
+        try {
+            retrieveUser(token, (error, user) => {
+                if (error) {
+                    logger.error(error)
+
+                    res.redirect('/error')
+                }
+
+                const { name, username } = user
+
+                // res.send(App({ title: 'My App', body: Landing({ name, username }), acceptCookies }))
+                res.render('landing', { name, username, acceptCookies} )
+            })
+        } catch (error) {
+            logger.error(error)
+
+            res.redirect('/error')
+        }
+
+    } else res.render('landing', { acceptCookies} )
+    // } else res.send(App({ title: 'My App', body: Landing(), acceptCookies }))
+}
