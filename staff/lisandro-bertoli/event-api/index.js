d0a671fd2d6134640d436721de95412e5a87ad72
Lisandro B <lisandro.bertoli@gmail.com>
2020-02-26 22:26:26 +0100

    initial commit

---
+++ staff/lisandro-bertoli/event-api/index.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+
+const { env: { PORT = 8080, NODE_ENV: env }, argv: [, , port = PORT] } = process
+
+const express = require('express')
+const winston = require('winston')
+const { registerUser, authenticateUser } = require('./routes')
+const { name, version } = require('./package')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const fs = require('fs')
+const path = require('path')
+
+const logger = winston.createLogger({
+    level: env === 'development' ? 'debug' : 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.File({ filename: 'server.log' })
+    ]
+})
+
+if (env !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }))
+}
+
+const jsonBodyParser = bodyParser.json()
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+const app = express()
+
+app.use(morgan('combined', { stream: accessLogStream }))
+
+app.post('/users', jsonBodyParser, registerUser)
+
+// app.get('/users')
+
+app.post('/users/auth', jsonBodyParser, authenticateUser)
+
+
+app.listen(port, () => logger.info(`server ${name} ${version} bergas comidas por Lisandro ${port}`))
+
+process.on('SIGINT', () => {
+    logger.info('lisandro come bergas')
+
+    process.exit(0)
+})
