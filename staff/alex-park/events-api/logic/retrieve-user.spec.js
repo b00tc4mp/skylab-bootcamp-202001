ed7854f5a742f30a8e857f205a5650f947fc31a5
Alex Park <alexparkvinyas@gmail.com>
2020-02-27 15:37:03 +0100

    added more testing

--- staff/alex-park/events-api/logic/retrieve-user.spec.js
+++ staff/alex-park/events-api/logic/retrieve-user.spec.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
 const { retrieveUser, authenticateUser } = require('.')
 const { expect } = require('chai')
+const { NotFoundError, NotAllowedError } = require('../errors')
 
 describe('retrieveUser', () => {
-    let token, name, surname, email
+    let id, name, surname, email
     beforeEach(async () => {
         name = 'Alex'
         surname = 'Park'
         email = 'pepitazo@gmail.com'
         password = '123'
-        token = await authenticateUser(email, password)
+        id = await authenticateUser(email, password)
     })
 
     it('should return the user on a successful user retrieve', () => {
-        retrieveUser(token)
+        retrieveUser(id)
             .then(user => {
                 expect(user.name === name).to.be.true
                 expect(user.surname === surname).to.be.true
@@ -21,24 +22,36 @@ describe('retrieveUser', () => {
             })
     })
 
-    it('should throw an error on a failed user retrieval', () => {
-        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-        expect (() => retrieveUser(token).to.throw(Error, 'user has not been retrieved'))
+    it('should throw a NotFoundError on a failed user retrieval', () => {
+        authenticateUser(email, password)
+            .catch(error => {
+                // expect(() => retrieveUser(error)).to.throw(NotFoundError, 'user has not been retrieved')
+                expect(error.message === 'user has not been retrieved').to.be.true
+                expect(error.status === 404).to.be.true
+                expect(error instanceof NotFoundError).to.be.true
+            })
+    })
+
+    it('should fail with a NotAllowedError on a deactivated user', () => {
+        authenticateUser('pepitazo10@gmail.com', '123')
+            .then(id =>
+                expect(() => retrieveUser(id)).to.throw(NotAllowedError, `user with id ${id} is deactivated`)
+            )
     })
 
     it('should fail on non-string or invalid token', () => {
 
-        token = Boolean(true)
-        expect(()=> retrieveUser(token)).to.throw(TypeError, `token ${token} is not a string`) 
+        id = Boolean(true)
+        expect(() => retrieveUser(id)).to.throw(TypeError, `id ${id} is not a string`)
 
-        token = Number(5)
-        expect(()=> retrieveUser(token)).to.throw(TypeError, `token ${token} is not a string`) 
+        id = Number(5)
+        expect(() => retrieveUser(id)).to.throw(TypeError, `id ${id} is not a string`)
 
-        token = Array(1,2,3)
-        expect(()=> retrieveUser(token)).to.throw(TypeError, `token ${token} is not a string`) 
+        id = Array(1, 2, 3)
+        expect(() => retrieveUser(id)).to.throw(TypeError, `id ${id} is not a string`)
 
-        token = ''
-        expect(()=> retrieveUser(token)).to.throw(Error, `token is empty`) 
+        id = ''
+        expect(() => retrieveUser(id)).to.throw(Error, `id is empty`)
 
     })
 })
