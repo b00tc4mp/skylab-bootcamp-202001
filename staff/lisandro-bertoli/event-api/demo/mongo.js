c3e3572baada71140c9d592a19b5dabf3eae34b3
Lisandro B <lisandro.bertoli@gmail.com>
2020-02-27 21:06:27 +0100

    mongo demo

---
+++ staff/lisandro-bertoli/event-api/demo/mongo.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
+    .then(client => {
+        const db = client.db('event')
+
+        const users = db.collection('users')
+
+        users.insertOne({ name: 'lala', surname: 'Lolo', email: 'jaadlo@grillo.com', password: 'ppp' })
+            .then(() => { const cursor = users.find(); return cursor })
+            .then(cursor => cursor.toArray())
+            .then(users => users.forEach(user => console.log(user)))
+
+        users.updateOne(
+            { name: "lala" },
+            { $set: { password: "111", email: "pepigri@gri.com" } }
+        ).then(result => console.log(result))
+            .then(() => users.deleteOne({ password: 'ppp' }))
+            .then(() => console.log('LAST ACTION:\n deleted'))
+            .catch(error => console.log('error', error))
+
+
+    })
