9ac5365a81167e98319091b4eb006cafffefd621
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-02-28 08:41:21 +0100

    register spec

--- staff/alex-martinez/events-api/logic/register-user.spec.js
+++ staff/alex-martinez/events-api/logic/register-user.spec.js
@@ -1,1 +1,27 @@
 // TODO
+const registerUser  = require('./register-user')
+const {expect} = require('chai')
+
+describe('registerUser', () => {
+    let name, surname, email, password
+
+    beforeEach(() => {
+        name = 'name-' + Math.random()
+        surname = 'surname-' + Math.random()
+        email =  'a@mail.com' 
+        password = 'password-' + Math.random()
+    })
+
+    it('should succeed on a new user', () => 
+        registerUser(name,surname,email,password)
+        .then(response =>{
+            expect(response).to.be.an('undefined')
+        })
+    )
+
+
+    describe('when user already exists', () => {
+    
+    })
+
+})
