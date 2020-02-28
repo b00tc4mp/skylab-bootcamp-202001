d11f39506efeae2fa55b0d6679a7d8d4b80ffca5
Alex Park <alexparkvinyas@gmail.com>
2020-02-28 20:09:51 +0100

    added some mongo changes to index

--- staff/alex-park/events-api/index.js
+++ staff/alex-park/events-api/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { env: { PORT = 8080, NODE_ENV: env }, argv: [, , port = PORT] } = process
+const { env: { PORT = 8080, NODE_ENV: env, MONGODB_URL }, argv: [, , port = PORT] } = process
 
 const express = require('express')
 const winston = require('winston')
@@ -11,39 +11,46 @@ const morgan = require('morgan')
 const fs = require('fs')
 const path = require('path')
 const { jwtVerifierMidWare } = require('./mid-wares')
+const { database } = require('./data')
 
-const logger = winston.createLogger({
-    level: env === 'development' ? 'debug' : 'info',
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: 'server.log' })
-    ]
-})
 
-if (env !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }))
-}
+database.connect(MONGODB_URL)
+    .then(() => {
 
-const jsonBodyParser = bodyParser.json()
+        const logger = winston.createLogger({
+            level: env === 'development' ? 'debug' : 'info',
+            format: winston.format.json(),
+            transports: [
+                new winston.transports.File({ filename: 'server.log' })
+            ]
+        })
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+        if (env !== 'production') {
+            logger.add(new winston.transports.Console({
+                format: winston.format.simple()
+            }))
+        }
 
-const app = express()
+        const jsonBodyParser = bodyParser.json()
 
-app.use(morgan('combined', { stream: accessLogStream }))
+        const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
-app.post('/users', jsonBodyParser, registerUser)
+        const app = express()
 
-app.post('/users/auth', jsonBodyParser, authenticateUser)
+        app.use(morgan('combined', { stream: accessLogStream }))
 
-app.get('/users', jwtVerifierMidWare, retrieveUser)
+        app.post('/users', jsonBodyParser, registerUser)
 
-app.listen(port, () => logger.info(`server ${name} ${version} up and running on port ${port}`))
+        app.post('/users/auth', jsonBodyParser, authenticateUser)
 
-process.on('SIGINT', () => {
-    logger.info('server abruptly stopped')
+        app.get('/users', jwtVerifierMidWare, retrieveUser)
 
-    process.exit(0)
-})
+        app.listen(port, () => logger.info(`server ${name} ${version} up and running on port ${port}`))
+
+        process.on('SIGINT', () => {
+            logger.info('server abruptly stopped')
+
+            process.exit(0)
+        })
+
+    })
--- staff/alex-park/events-api/logic/register-user.js
+++ staff/alex-park/events-api/logic/register-user.js
@@ -1,35 +1,27 @@
 const { validate } = require('../utils')
-const { users } = require('../data')
-const fs = require('fs').promises
-const path = require('path')
-const { v4: uuid } = require('uuid')
-const { MongoClient } = require('mongodb')
+const { database } = require('../data')
 
-const client = new MongoClient("mongodb://localhost:27017", { useUnifiedTopology: true })
 
-client.connect()
-.then(() => {
-    const db = client.db('events')
-    const users = db.collection('users')
-
+const client = database.connect()
+const users = database.collection('users')
+.then(()=> {
+    module.exports = (name, surname, email, password) => {
+        validate.string(name, 'name')
+        validate.string(surname, 'surname')
+        validate.string(email, 'email')
+        validate.email(email)
+        validate.string(password, 'password')
     
+        let user = users.find(user => user.email === email)
+    
+        if (user) throw new Error(`user with email ${email} already exists`)
+    
+        user = { id: uuid(), name, surname, email, password, created: new Date }
+    
+        users.push(user)
+    
+        return fs.writeFile(path.join(__dirname, '../data/users.json'), JSON.stringify(users, null, 4))
+    }
+
 })
 
-
-module.exports = (name, surname, email, password) => {
-    validate.string(name, 'name')
-    validate.string(surname, 'surname')
-    validate.string(email, 'email')
-    validate.email(email)
-    validate.string(password, 'password')
-
-    let user = users.find(user => user.email === email)
-
-    if (user) throw new Error(`user with email ${email} already exists`)
-
-    user = { id: uuid(), name, surname, email, password, created: new Date }
-
-    users.push(user)
-
-    return fs.writeFile(path.join(__dirname, '../data/users.json'), JSON.stringify(users, null, 4))
-}
