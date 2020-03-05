63a5746c87f2a48134b42b95fc0b7edc07cb98a2
RPXIC <rpc2works@gmail.com>
2020-03-05 18:55:58 +0100

    added bcrypt

--- staff/ruben-ponce/events-api/logic/register-user.js
+++ staff/ruben-ponce/events-api/logic/register-user.js
@@ -1,9 +1,10 @@
 const { validate } = require('events-utils')
 const { models: { User } } = require('events-data')
 const { NotAllowedError } = require('events-errors')
+const bcrypt = require('bcryptjs')
 
 module.exports = (name, surname, email, password) => {
-    debugger
+
     validate.string(name, 'name')
     validate.string(surname, 'surname')
     validate.email(email)
@@ -12,7 +13,10 @@ module.exports = (name, surname, email, password) => {
     return User.findOne({ email })
         .then(user => {
             if (user) throw new NotAllowedError(`user with email ${email} already exists`)
-
+            return bcrypt.hash(password, 10)            
+        })
+        .then(password => {
+            
             user = new User({ name, surname, email, password, created: new Date })
 
             return user.save()
