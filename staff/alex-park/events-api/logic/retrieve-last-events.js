059ab71497db2bbf7270de64037a4bb46725056c
Alex Park <alexparkvinyas@gmail.com>
2020-03-06 09:54:21 +0100

    minor change

--- staff/alex-park/events-api/logic/retrieve-last-events.js
+++ staff/alex-park/events-api/logic/retrieve-last-events.js
@@ -1,12 +1,12 @@
 const { models: { Event } } = require('events-data')
 
-module.exports = () => Event.find({ date: { $gte: new Date } })
+module.exports = () => Event.find({ date: { $gte: new Date } }).populate('publisher', name)
     .lean()
     .then(events => {
         // sanitize
         events.forEach(event => {
             event.id = event._id.toString()
-
+            
             delete event._id
 
             event.publisher = event.publisher.toString()
