ca27702bab3721e4f454d6d282dc92dfaf85c2b3
Lisandro B <lisandro.bertoli@gmail.com>
2020-03-07 15:57:10 +0100

    removed file

--- staff/lisandro-bertoli/event-api/routes/handlers/user.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const router = Router()
-const asycHandler = require('../../handlers/async-handler')
-
-module.exports = router
-    .post('/', asycHandler(async (req, res, next) => {
-        await registerUser(req.body)
-
-    }))
