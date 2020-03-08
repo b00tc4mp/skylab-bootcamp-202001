428260cc75a46c72eb7dc689ccd68e19a3af7a0c
Alex Park <alexparkvinyas@gmail.com>
2020-03-08 16:23:21 +0100

    added first api files

---
+++ staff/alex-park/poopinion/poopinion-api/logic/register-user.js
@@ -0,0 +1,27 @@
+const { validate } = require('poopinion-utils')
+const { models: { User } } = require('poopinion-data')
+const { NotAllowedError } = require('poopinion-errors')
+const bcrypt = require('bcryptjs')
+
+module.exports = (name, surname, email, password, age, gender) => {
+    validate.string(name, 'name')
+    validate.string(surname, 'surname')
+    validate.string(email, 'email')
+    validate.email(email)
+    validate.string(password, 'password')
+
+
+    return User.findOne({ email })
+        .then(user => {
+            if (user) throw new NotAllowedError(`user with email ${email} already exists`)
+
+            return bcrypt.hash(password, 10)
+        })
+        .then(password => {
+            user = new User({ name, surname, email, age, gender, password, created: new Date })
+
+            return user.save()
+        })
+        .then(() => { })
+
+}
---
+++ staff/alex-park/poopinion/poopinion-api/routes/index.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const {
+    registerUser,
+    authenticateUser,
+    retrieveUser
+} = require('./handlers')
+const bodyParser = require('body-parser')
+const { jwtVerifierMidWare } = require('../mid-wares')
+const jsonBodyParser = bodyParser.json()
+
+const router = new Router()
+
+router.post('/users', jsonBodyParser, registerUser)
+
+router.post('/users/auth', jsonBodyParser, authenticateUser)
+
+router.get('/users', jwtVerifierMidWare, retrieveUser)
