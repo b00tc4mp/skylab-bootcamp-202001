f328421d713343ef7be255a6272b44101e632fa8
Lisandro B <lisandro.bertoli@gmail.com>
2020-03-09 09:46:05 +0100

    add publish comment logic

---
+++ staff/lisandro-bertoli/sick-parks/sick-parks-api/logic/publish-comment.js
@@ -0,0 +1,25 @@
+const { validate } = require('sick-parks-utils')
+const { models: { Park, User, Comment } } = require('sick-parks-data')
+const { NotFoundError } = require('sick-parks-errors')
+
+module.exports = async ({ userId, parkId }, body) => {
+    validate.string(userId, 'userId')
+    validate.string(parkId, 'parkId')
+    validate.string(body, 'body')
+
+    const user = await User.findById(userId)
+
+    if (!user) throw new NotFoundError(`user with id ${userId} does not exist`)
+
+    const park = await Park.findById(parkId)
+
+    if (!park) throw new NotFoundError(`park with id ${parkId} does not exist`)
+
+    const comment = await Comment.create({ postedBy: userId, body })
+
+    park.comments.push(comment)
+
+    await park.save()
+
+    return comment
+}
--- staff/lisandro-bertoli/sick-parks/sick-parks-data/schemas/comment.js
+++ staff/lisandro-bertoli/sick-parks/sick-parks-data/schemas/comment.js
@@ -3,5 +3,5 @@ const { Schema, Types: { ObjectId } } = require('mongoose')
 module.exports = new Schema({
     postedBy: { type: ObjectId, required: true, ref: 'User' },
     body: { type: String, required: true },
-    date: { type: Date, required: true }
+    date: { type: Date, default: Date.now }
 }) 
