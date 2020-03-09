91fb235c3a638548d186ed0fc896e3ae908ab5c4
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-03-09 18:24:54 +0100

    change to camel case

--- staff/alex-martinez/staycar/staycar-data/models/parking.js
+++ staff/alex-martinez/staycar/staycar-data/models/parking.js
@@ -1,8 +1,8 @@
 const {Schema, model } = require('mongoose')
 
 const ParkingSchema = new Schema({
-    plates_total: {type : Number, required: true },
-    plates_occupied: { type: Number, required: true }
+    totalLots: {type : Number, required: true },
+    occupiedLots: { type: Number, required: true }
 })
 
 module.exports = model('Parking', ParkingSchema)
