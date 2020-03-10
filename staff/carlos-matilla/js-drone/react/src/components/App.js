e9a83df43e78dde7ab845a189f26283173bd3aa5
Carlos Matilla <carlosmatilla@MacBook-Pro-de-Carlos-2.local>
2020-03-10 11:01:41 +0100

    charts finished

--- staff/carlos-matilla/js-drone/react/src/components/App.js
+++ staff/carlos-matilla/js-drone/react/src/components/App.js
@@ -4,6 +4,15 @@ import './App.css';
 import socket from '../socket';
 import { gamepadConnect, gamepadDisconnect, check } from "../logic/gamepad";
 import {keyDown, keyUp} from '../logic/keyboard'
+import "chartjs-plugin-streaming";
+
+
+import {Line, Bar} from 'react-chartjs-2';
+
+
+
+
+
 
 
 
@@ -30,6 +39,101 @@ function DroneState() {
   return droneState;
 }
 
+const temperature = {
+  datasets: [
+    {
+      label: "Hight Temp",
+      borderColor: "rgb(245,85,54)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+      
+      data: []
+    },
+    {
+      label: "Low Temp",
+      borderColor: "rgb(2,143,204)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+     
+      data: []
+    }
+  ]
+};
+
+
+const speed = {
+  datasets: [
+    {
+      label: "Vx",
+      borderColor: "rgb(2,143,204)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+      // borderDash: [8, 8],
+      data: []
+    },
+    {
+      label: "Vy",
+      borderColor: "rgb(255,255,255)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+      // borderDash: [8, 4],
+      data: []
+    },
+    {
+      label: "Vz",
+      borderColor: "rgb(245,85,54)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+      // borderDash: [8, 4],
+      data: []
+    }
+  ]
+};
+
+
+const aceleration = {
+  datasets: [
+    {
+      label: "Ax",
+      borderColor: "rgb(245,85,54)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+     
+      data: []
+    },
+    {
+      label: "Ay",
+      borderColor: "rgb(255,255,255)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+      
+      data: []
+    },
+    {
+      label: "Az",
+      borderColor: "rgb(2,143,204)",
+      backgroundColor: "rgba(0, 0, 0, 0.1)",
+      lineTension: 0.4,
+      
+      data: []
+    }
+  ]
+};
+
+const height = {
+  datasets: [
+    {
+      label: "Altura",
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      lineTension: 0.4,
+      
+      
+      data: []
+    }
+    
+  ]
+};
 
 function App() {
 
@@ -51,12 +155,127 @@ function App() {
   const { pitch, roll, yaw, vgx, vgy, vgz, templ, temph, tof, h, bat, baro, time, agx, agy, agz } = state
 
 
+
+  const heightGra = Number(tof)
+
+  
+  
+ 
+  const temperatureOpt = {
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            onRefresh: function() {
+              temperature.datasets[0].data.push({
+                x: Date.now(),
+                y: Number(temph)
+              });
+              temperature.datasets[1].data.push({
+                x: Date.now(),
+                y: Number(templ)
+              });
+            },
+            
+            
+            refresh: 300
+          }
+        }
+      ]
+    }
+  };
+
+
+  const speedOpt = {
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            onRefresh: function() {
+              speed.datasets[0].data.push({
+                x: Date.now(),
+                y: Number(vgx)
+              });
+              speed.datasets[1].data.push({
+                x: Date.now(),
+                y: Number(vgy)
+              });
+              speed.datasets[2].data.push({
+                x: Date.now(),
+                y: Number(vgz)
+              });
+            },
+            
+            
+            refresh: 300
+          }
+        }
+      ]
+    }
+  };
+
+
+  const acelerationOpt = {
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            onRefresh: function() {
+              aceleration.datasets[0].data.push({
+                x: Date.now(),
+                y: Number(agx)
+              });
+              aceleration.datasets[1].data.push({
+                x: Date.now(),
+                y: Number(agy)
+              });
+              aceleration.datasets[2].data.push({
+                x: Date.now(),
+                y: Number(agz)
+              });
+            },
+            
+            
+            refresh: 300
+          }
+        }
+      ]
+    }
+  };
+
+  const heightOpt = {
+    scales: {
+      xAxes: [
+        {
+          type: "realtime",
+          realtime: {
+            onRefresh: function() {
+              height.datasets[0].data.push({
+                x: Date.now(),
+                y: Number(tof)
+              });
+            },
+            
+            
+            refresh: 300
+          }
+        }
+      ]
+    }
+  };
+  
+  
+
+
   return (
     <div className="Drone">
       <header className="App-header">
-        {<img src={logo} className="App-logo" alt="logo" />}
+        {/* {<img src={logo} className="App-logo" alt="logo" />}
 
-        {<h3>{status}</h3>}
+        {<h3>{status}</h3>} */}
         <section className="telemetria">
           <div><p>Pitch</p><p>{pitch}</p></div>
           <div><p>Roll</p><p>{roll}</p></div>
@@ -75,6 +294,22 @@ function App() {
           <div><p>AC en Y</p><p>{agy}</p></div>
           <div><p>AC en Z</p><p>{agz}</p></div>
         </section>
+        <section className="charts">
+          <div>
+            <Line data={height} options={heightOpt} />
+          </div>
+          <div>
+            <Line data={temperature} options={temperatureOpt}  />
+          </div>
+          <div>
+            <Line data={speed} options={speedOpt} />
+          </div>
+          <div>
+            <Line data={aceleration} options={acelerationOpt}/>
+          </div>
+        {/* <Bar data={temperature} options={temperatureOpt}/> */}
+        </section>
+        
 
       </header>
 
