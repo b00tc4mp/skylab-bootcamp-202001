b9ab170514daa0d13d0595202913309b346d25b8
RPXIC <rpc2works@gmail.com>
2020-03-16 20:23:15 +0100

    starting app

--- staff/ruben-ponce/simonline/simonline-app/src/components/App.js
+++ staff/ruben-ponce/simonline/simonline-app/src/components/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { /*useState, Fragment*/ } from 'react'
 import logo from './logo.svg';
+// import { Login } from './'
 import './App.css';
+
 
 function App() {
   return (
