f0a6b02bf4c6dec3cc67cc4fd42573682552b7c5
Lisandro B <lisandro.bertoli@gmail.com>
2020-03-19 11:42:35 +0100

    add search logic

--- staff/lisandro-bertoli/sick-parks/sick-parks-app/src/logic/index.js
+++ staff/lisandro-bertoli/sick-parks/sick-parks-app/src/logic/index.js
@@ -1,9 +1,11 @@
 import registerUser from './register-user'
 import login from './login'
 import isLoggedIn from './is-logged-in'
+import retrieveUser from './retrieve-user'
 
 export {
     registerUser,
     login,
-    isLoggedIn
+    isLoggedIn,
+    retrieveUser
 }
---
+++ staff/lisandro-bertoli/sick-parks/sick-parks-app/src/logic/search-parks.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const { NotFoundError } = require('../sick-parks-errors')
+const { validate } = require('../sick-parks-utils')
+const fetch = require('node-fetch')
+const API_URL = process.env.REACT_APP_API_URL
+
+export default searchParks = ({ query, location }) => {
+    validate.string(query, 'query')
+
+    return (async () => {
+        const response = await fetch(`${API_URL}/parks?q=${query}&_location=${location}`)
+
+        const data = await response.json()
+
+        const { error, results } = data
+
+        if (error) throw new Error(error)
+        return results
+
+    })()
+
+}
