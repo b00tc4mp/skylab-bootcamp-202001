23c10995ca805f9e0df677dd5fd2e634f76c7509
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-03-24 10:26:21 +0100

    correction  ortographic errors

--- staff/alex-martinez/staycar/staycar-app/src/components/MapItem.js
+++ staff/alex-martinez/staycar/staycar-app/src/components/MapItem.js
@@ -5,6 +5,6 @@ export default ({lot}) => {
 
     return <div className={`plates__plate--${lot.status}`}>
         <Car className="plates__car"/>
-        <p className="plates__text">Plate Numer: <span className="plates__number">{lot.number}</span></p>
+        <p className="plates__text">Plate Number: <span className="plates__number">{lot.number}</span></p>
     </div>
 }
