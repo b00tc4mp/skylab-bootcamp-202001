9dfcf183b4a87f7dcc63ab08a1c8274c9076e6ad
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-03-26 09:56:14 +0100

    update-parking

---
+++ staff/alex-martinez/staycar/staycar-api/logic/update-parking.js
@@ -0,0 +1,71 @@
+const { validate } = require("staycar-utils")
+const { models: { User, Parking } } = require("staycar-data")
+const { NotFoundError } = require("staycar-errors")
+
+module.exports = (id, parkingName,rate, totalLots) => {
+  validate.string(id, "id")
+  validate.string(parkingName, "parking name")
+  validate.type(totalLots, "total lots", Number)
+  validate.type(rate, "rate", Number)
+  
+  if (totalLots <= 0) throw new TypeError("the number of lots must be bigger than 0")
+  
+  if(rate <= 0) throw new TypeError("rate must be bigger than 0")
+
+  return (async () => {
+    
+    const user = await User.findById( id )
+    
+    if (!user) throw new NotFoundError(`user with id ${id} does not exist`)
+
+    const parking = await Parking.findOne({ parkingName})
+    
+    if (!parking)
+      throw new NotFoundError(`parking with name ${parkingName} does not exist`)
+    
+    let lots = []
+
+    for (let i = 1; i <= totalLots; i++) {
+        let lot = {}
+        lot.number = i
+        lot.status = false
+
+        lots.push(lot)
+    }
+
+    await parking.updateOne({rate, totalLots, lots})
+
+    //await parking.save()
+
+    /* let { lots } = parking
+
+    parking.totalLots = number
+    
+    if (number < lots.length) {
+      let difLots = lots.length - number
+
+      for (let i = lots.length+1; i >= difLots; i--) {
+        lots.pop(i)
+      }
+
+      parking.rate = rate
+
+      await parking.save()
+      return 
+    } 
+
+    for (let i = lots.length + 1; i <= number; i++) {
+      let lot = {}
+      lot.number = i
+      lot.status = false
+
+      lots.push(lot)
+    }
+
+    parking.rate = rate
+
+    await parking.save()
+
+    return  */
+  })();
+};
