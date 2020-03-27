de7a8094e89ed22d3f254393f1576fbe60af39c7
Aurora Pérez Alonso <aurora.perez1993@gmail.com>
2020-03-27 13:28:02 +0100

    change to database

---
+++ staff/aurora-perez/pill-o-clock/pilloclockapp/config.js
@@ -0,0 +1,6 @@
+
+export default {
+    REACT_APP_API_URL = 'http://localhost:8085/api',
+    REACT_APP_TEST_MONGODB_URL = 'mongodb://localhost:27017/test-events',
+    REACT_APP_TEST_JWT_SECRET = my cat is a demon,
+}
