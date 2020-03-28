9680e75d75c040ccb9247d3a432b7ee4ff7331b7
Alex Martinez <alex.martinez.jativa@gmail.com>
2020-03-28 08:06:39 +0100

    delete lot model

--- staff/alex-martinez/staycar/staycar-data/models/lot.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model } = require('mongoose')
-
-const LotSchema = new Schema({
-    plateNumber: {type : String, required: true, trim: true, unique: true},
-    occupied: { type: Boolean, required: true, default: false}
-})
-
-module.exports = model('Lot', LotSchema)
