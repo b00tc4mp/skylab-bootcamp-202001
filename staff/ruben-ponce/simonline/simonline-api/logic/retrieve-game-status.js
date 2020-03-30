ab92fac18cc783ef18cf5ec8de7c3183331e4e41
RPXIC <rpc2works@gmail.com>
2020-03-31 01:40:59 +0200

    doing retrieve status

--- staff/ruben-ponce/simonline/simonline-api/logic/retrieve-game-status.js
+++ staff/ruben-ponce/simonline/simonline-api/logic/retrieve-game-status.js
@@ -1,88 +1,94 @@
-const validate = require("simonline-utils/validate")
-const { models: { User, Game } } = require("simonline-data")
-const { NotFoundError } = require("simonline-errors")
-let util = require('util')
+const validate = require("simonline-utils/validate");
+const {
+  models: { User, Game }
+} = require("simonline-data");
+const { NotFoundError } = require("simonline-errors");
 
 /**
  * Retrieve game status from database
- * 
+ *
  * @param {string} playerId unique user id
  * @param {string} id of game
- * 
+ *
  * @returns {Promise<Object>} status of game
- * 
+ *
  * @throws {NotFoundError} when not found player
  * @throws {NotFoundError} when not found game
  * @throws {NotFoundError} when player not joined on this game
  */
 
 module.exports = (playerId, gameId) => {
-  validate.string(playerId, "playerId")
-  validate.string(gameId, "gameId")
+  validate.string(playerId, "playerId");
+  validate.string(gameId, "gameId");
 
   return User.findById(playerId)
     .then(user => {
-        if(!user) throw new NotFoundError(`player with id ${playerId} not found`)
+      if (!user)
+        throw new NotFoundError(`player with id ${playerId} not found`);
     })
     .then(() => {
-        return Game.findById(gameId)
-          .then(game => {
-            if (!game) throw new NotFoundError(`game with id ${gameId} not found`)
-            
-            const { status, turnTimeout } = game
+      return Game.findById(gameId).then(game => {
+        if (!game) throw new NotFoundError(`game with id ${gameId} not found`);
 
-            if(!game.players.includes(playerId)) throw new NotFoundError(`player ${playerId}, not joined on game`)
-      
-            if (status === "started") {
-      
-              const timeNow = new Date()
-      
-              const elapsedTime = (timeNow - game.turnStart) / 1000
-              
-              /* when has passed countdown on turn */
-              if (elapsedTime > turnTimeout) {
-                let pause = util.promisify((a, f) => setTimeout(f, a))
-                let delay = Math.floor(Math.random() * (300 - 250 + 1) + 250)
-                console.log(delay)
-                pause(delay)
+        const { status, turnTimeout } = game;
+        if (game.players.every(player => player.toString() !== playerId))
+        throw new NotFoundError(`player ${playerId}, not joined on game`);
+        
+        if (status === "started") {
+          debugger
 
-                if (game.watching.includes(game.currentPlayer)) {
-                  return game
-                } else {
-                  debugger
-                  game.watching.push(game.currentPlayer)
-                  console.log(timeNow)
-                  console.log(game.currentPlayer)
-                }
-                let j = game.players.indexOf(game.currentPlayer)
-      
-                if (game.players.length === (game.watching.length + 1)) {
-                  //change winner player to current player
-                  for (let i = j; i < game.players.length; i++) {
-                    if(!game.watching.includes(game.players[i]) && game.currentPlayer !== game.players[i]) {
-                      game.currentPlayer = game.players[i]
-                      game.status = 'finished'
-                      game.save()
-                      return game
-                    }
-                    if(!game.players[i+1]) i = -1
-                  }
-                }
+          const timeNow = new Date();
 
-                for (let i = j; i < game.players.length; i++) {
-                  if(!game.watching.includes(game.players[i]) && game.currentPlayer !== game.players[i]) {
-                    game.currentPlayer = game.players[i]
-                    game.turnStart = new Date()
-                    game.save()
-                    return game
-                  }
-                  if(!game.players[i+1]) i = -1
-                }
-                /* Matching betwen combinationPlayer and combinationGame */
+          const elapsedTime = (timeNow - game.turnStart) / 1000;
+
+          /* when has passed countdown on turn */
+          if (elapsedTime > turnTimeout) {
+            const playerNotWatching = playerId =>
+              game.watching.every(
+                player => player.toString() !== playerId.toString()
+              );
+
+            const { currentPlayer } = game;
+
+            game.watching.push(currentPlayer);
+
+            const currentPlayerIndex = game.players.indexOf(currentPlayer);
+
+            for (
+              let i = currentPlayerIndex + 1;
+              i < game.players.length &&
+              game.currentPlayer.toString() === currentPlayer.toString();
+              i++
+            ) {
+              const potentialNextPlayer = game.players[i];
+
+              if (playerNotWatching(potentialNextPlayer)) {
+                game.currentPlayer = potentialNextPlayer;
+                game.turnStart = new Date();
               }
             }
-            return game
-          })
-          .then(game => game)
-    })
-}
+
+            for (
+              let i = 0;
+              i < currentPlayerIndex &&
+              game.currentPlayer.toString() === currentPlayer.toString();
+              i++
+            ) {
+              const potentialNextPlayer = game.players[i];
+
+              if (playerNotWatching(potentialNextPlayer)) {
+                game.currentPlayer = potentialNextPlayer;
+                game.turnStart = new Date();
+              }
+            }
+
+            if (game.currentPlayer.toString() === currentPlayer.toString())
+              game.status = "finished";
+
+            return game.save();
+          }
+        }
+        return game;
+      });
+    });
+};
---
+++ staff/ruben-ponce/simonline/simonline-doc/README.md
@@ -0,0 +1,23 @@
+# Simonline
+## Introduction
+Multiplayer memory game
+
+![](img/simon.gif)
+
+## Functional description
+
+Simon is an electronic game that was very successful during the 80s. It has a disk shape, on one of its faces you can see four quadrants, each with a color: green, red, blue and yellow in its original version.
+
+The game randomly illuminates the colored quadrants. After waiting, the user must enter the sequence shown in the correct order, using his visual memory. If successful, it will respond with a longer sequence for the next player, and so on. If it fails, the user will wait for the other players to finish until there is only one left
+
+### Use cases
+
+![titulo](img/use-cases.jpg) 
+
+## Tecnical description
+
+### Workflow
+![titulo](url)
+
+## Blocks
+![titulo](url)
