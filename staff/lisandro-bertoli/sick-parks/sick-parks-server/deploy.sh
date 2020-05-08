7eab1db7af633f728d4de53fae9611bf88ab8dfa
Lisandro B <lisandro.bertoli@gmail.com>
2020-05-08 15:04:23 +0200

    add deploy settings

---
+++ staff/lisandro-bertoli/sick-parks/sick-parks-server/deploy.sh
@@ -0,0 +1,4 @@
+rsync -va --del ../sick-parks-api/ ~/sick-parks-deploy/sick-parks-api/
+rsync -va --del ../sick-parks-data/ ~/sick-parks-deploy/sick-parks-data/
+rsync -va --del ../sick-parks-utils/ ~/sick-parks-deploy/sick-parks-utils/
+rsync -va --del ../sick-parks-errors/ ~/sick-parks-deploy/sick-parks-errors/
